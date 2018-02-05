d96b24f8353cdc8da8739d4031d70b9d27e1b7b5
Layon Martins <layonmartins03@gmail.com>
2018-02-05 16:11:02 -0200

    fixed error in activity act_cad_contatos layout:height match_parent don't work.

--- app/src/main/java/com/layon/agendacontato/ActContato.java
+++ app/src/main/java/com/layon/agendacontato/ActContato.java
@@ -2,12 +2,26 @@ package com.layon.agendacontato;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.*;
+import android.widget.*;
+import android.content.*;
 
-public class ActContato extends AppCompatActivity {
+public class ActContato extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageButton btnAdicionar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_contato);
+
+        btnAdicionar = (ImageButton) findViewById(R.id.btnAdicionar);
+        btnAdicionar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent it = new Intent(this, ActCadContatos.class);
+        startActivity(it);
     }
 }
