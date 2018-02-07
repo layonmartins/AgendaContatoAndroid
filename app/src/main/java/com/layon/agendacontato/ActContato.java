56e11d3425f7ba157cc7d5ccede9c8829660cd1b
Layon Martins <layonmartins03@gmail.com>
2018-02-07 13:59:51 -0200

    Using SQL to create and read table CONTATOS

--- app/src/main/java/com/layon/agendacontato/ActContato.java
+++ app/src/main/java/com/layon/agendacontato/ActContato.java
@@ -1,16 +1,21 @@
 package com.layon.agendacontato;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.*;
 import android.widget.*;
 import android.content.*;
+import android.database.sqlite.*;
+import android.database.*;
 
 public class ActContato extends AppCompatActivity implements View.OnClickListener {
 
     private ImageButton btnAdicionar;
     private EditText edtPesquisa;
     private ListView lstContatos;
+    private DataBase dataBase;
+    private SQLiteDatabase conn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,21 @@ public class ActContato extends AppCompatActivity implements View.OnClickListene
         edtPesquisa = (EditText)findViewById(R.id.edtPesquisa);
         lstContatos = (ListView)findViewById(R.id.lstContatos);
         btnAdicionar.setOnClickListener(this);
+        try {
+            dataBase = new DataBase(this);
+            conn = dataBase.getReadableDatabase();
+
+            AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+            dlg.setMessage("Conexão criada com sucesso!");
+            dlg.setNeutralButton("OK", null);
+            dlg.show();
+
+        }catch(SQLException ex){
+            AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+            dlg.setMessage("Erro ao criar o banco: " + ex.getMessage());
+            dlg.setNeutralButton("OK", null);
+            dlg.show();
+        }
 
     }
 
---
+++ app/src/main/java/com/layon/agendacontato/DataBase.java
@@ -0,0 +1,21 @@
+package com.layon.agendacontato;
+
+import android.content.Context;
+import android.database.sqlite.*;
+
+public class DataBase extends SQLiteOpenHelper{
+
+    public DataBase (Context context){
+        super(context, "AGENDA", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(ScriptSQL.getCreateContato());
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
